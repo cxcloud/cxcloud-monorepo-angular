2941a29c32207aeb3c5d0963f6628ebcd3ae71b1
Nadia Kolodyazhnaya <nadia.kolodyazhnaya@gmail.com>
2019-07-17 16:09:25 +0300

    Update configurations and yaml files; Add project to workspace;

--- packages/package-commerce/config/development.js
+++ packages/package-commerce/config/development.js
@@ -1,6 +1,23 @@
+require('dotenv').config({
+  path: require('find-config')('.env')
+});
+
 module.exports = {
-  port: 4003,
-  debug: {
-    logLevel: 'debug'
+  store: {
+    defaultCurrency: 'EUR',
+    supportedCurrencies: ['EUR', 'USD', 'GBP']
+  },
+  commerceTools: {
+    authHost: `${process.env.CTP_AUTH_URL}`,
+    apiHost: `${process.env.CTP_API_URL}`,
+    projectKey: process.env.CTP_PROJECT_KEY,
+    admin: {
+      clientId: process.env.CTP_CLIENT_ID,
+      clientSecret: process.env.CTP_CLIENT_SECRET
+    },
+    user: {
+      clientId: process.env.CTP_USER_CLIENT_ID,
+      clientSecret: process.env.CTP_USER_CLIENT_SECRET
+    }
   }
 };
