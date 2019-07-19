8d5072855293dbeea3431b82eee8af38a775bf70
Nadia Kolodyazhnaya <nadia.kolodyazhnaya@gmail.com>
2019-07-19 10:57:27 +0300

    Update configs;

--- packages/package-content/config/default.js
+++ packages/package-content/config/default.js
@@ -1,6 +1,16 @@
+require('dotenv').config({
+  path: require('find-config')('.env')
+});
+
 module.exports = {
   port: 4003,
   debug: {
     logLevel: 'debug'
+  },
+  contentful: {
+    sdkConfig: {
+      space: process.env.CONTENTFUL_SPACE_ID,
+      accessToken: process.env.CONTENTFUL_DELIVERY_API_TOKEN
+    }
   }
 };
--- packages/package-content/config/development.js
+++ packages/package-content/config/development.js
@@ -1,12 +1,1 @@
-require('dotenv').config({
-  path: require('find-config')('.env')
-});
-
-module.exports = {
-  contentful: {
-    sdkConfig: {
-      space: process.env.CONTENTFUL_SPACE_ID,
-      accessToken: process.env.CONTENTFUL_DELIVERY_API_TOKEN
-    }
-  }
-};
+module.exports = {};
