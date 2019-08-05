ed9b6b5759d26938480cc1a7b12c389fe43d9cad
mpxy <mpxy03@gmail.com>
2019-08-05 11:24:58 +0300

    Updated environment.ts

--- packages/package-frontend/src/environments/environment.ts
+++ packages/package-frontend/src/environments/environment.ts
@@ -8,15 +8,15 @@ export const environment = {
   production: false,
   siteName: 'CXCloud DEV',
   commerce: {
-    apiUrl: 'http://localhost:4003/api/commerce/v1',
+    apiUrl: 'http://commerce.localhost/api/commerce/v1',
     indexName: 'dev_COMMERCE'
   },
   content: {
-    apiUrl: 'http://localhost:4003/api/content/v1',
+    apiUrl: 'http://content.localhost/api/content/v1',
     indexName: 'dev_CONTENT'
   },
   auth: {
-    apiUrl: 'http://localhost:4003/api/commerce/v1/auth'
+    apiUrl: 'http://commerce.localhost/api/commerce/v1/auth'
   },
   search: {
     apiUrl: 'https://demo.cxcloud.com/api/search/v1'
